0a46f0ac5b847eb5d588583866d8e8876d3a69e9
Alex Chang <NecroHacks@gmail.com>
2023-05-11 08:53:08 -0400

    added more GUI fucntionality

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -37,9 +37,12 @@ public class Blackjack {
     public static int bet;
     public static int bal = 100;
 
+    public static JFrame frame;
+
     public static void main(String[] args) {}
 
-    public static void game(JFrame frame){
+    public static void game(JFrame f){
+        frame = f;
         reset();
         System.out.println(cards);
         System.out.println(num);
@@ -51,6 +54,8 @@ public class Blackjack {
 
         //player
         //draws two cards
+        
+
         
         // hand.add(chooseCard());
         // hand.add(chooseCard());
@@ -68,7 +73,7 @@ public class Blackjack {
         frame.add(betDisplay);
         frame.add(moneys);
         betDisplay.setText(""+bet);
-        moneys.setText(""+bal);
+        moneys.setText("Ballance: "+bal);
 
         //System.out.println("Press enter to continue...");
         JButton confirm = new JButton("Confirm");
@@ -99,10 +104,17 @@ public class Blackjack {
                     }
                     else{
                         bet = Integer.parseInt(totalBet.getText());
-                        betDisplay.setText(""+bet);
+                        betDisplay.setText("Bet: "+bet);
+                        moneys.setText("Ballance: "+(bal-bet));
                         confirm.setVisible(false);
                         totalBet.setVisible(false);
                         prompt.setVisible(false); 
+                        round();
+                        JLabel ace = new JLabel(new ImageIcon("C:/Users/necro/vsproj/src/smol-ace-1.png"));
+                        ace.setBounds(0,0,400,400);
+                        frame.add(ace);
+                        //ace.setVisible(true);
+                        ace.setText("");
                     }
                 }
                 catch(Exception ex){
@@ -115,15 +127,14 @@ public class Blackjack {
 
     }
 
-    public static void init(){
+    // public static void init(){
 
-    }
+    // }
 
     public static void round(){
-        //init bet
 
 
-
+        //resets the game
         reset();
         total = 0;
         botTotal = 0;
@@ -136,7 +147,7 @@ public class Blackjack {
         System.out.println(hand + " " + total);
         System.out.println(botHand + " " + botTotal);
 
-        while(total < 21){
+        /*while(total < 21){
             if(!play()){
                 break;
             }
@@ -181,8 +192,8 @@ public class Blackjack {
 
             }
         }
-
-        round();
+        
+        round();*/
     
     }
     
@@ -191,10 +202,12 @@ public class Blackjack {
      * @return A random card within the deck, and removes that card for future pulls.
      */
     public static String chooseCard(boolean player){
+        
         String card;
         int i = rand.nextInt(num.size()-1);
         card = cards.get(num.get(i));
         //total += value.get(num.get(i));
+
         if(player){ 
             calcTotal(value.get(num.get(i)));
         }
@@ -204,6 +217,7 @@ public class Blackjack {
         num.remove(i);
         //System.out.println(num);
         return card;
+
     } 
 
     /**
@@ -267,8 +281,13 @@ public class Blackjack {
     }
 
     //Starts the play
-    public static boolean play(){   
-        System.out.println("Hit(h) or Stand(s)?");
+    public static boolean play(){
+        JButton hit = new JButton("Hit");
+        JButton stand = new JButton("Stand");
+        
+        hit.setBounds(null);
+        
+        //System.out.println("Hit(h) or Stand(s)?");
         String act = in.nextLine();
         if(act.toLowerCase().equals("h")){
             hand.add(chooseCard(true));
@@ -285,20 +304,21 @@ public class Blackjack {
     }
 
     //I feel like these are self explanitory
+    //TODO: changes sysouts to display on screen
     public static void win(){
-        System.out.println("You win!");
+        //System.out.println("You win!");
         //Win bet x2
         bal += bet;
     }
 
     public static void lose(){
-        System.out.println("You lose");
+        //System.out.println("You lose");
         //Lose bet
         bal -= bet;
     }
 
     public static void tie(){
-        System.out.println("You tied");
+        //System.out.println("You tied");
         //Return ballance (do nothing)
     }
 
@@ -307,13 +327,17 @@ public class Blackjack {
      */ 
     public static void reset(){
         String c;
+
         System.out.println(color.Reset);
+
         hand.clear();
         botHand.clear();
         num.clear();
         cards.clear();
+
         ace = 0;
         botAce = 0;
+
         for(int i = 0; i<suits.size(); i++){
             for(int j = 1; j<=13; j++){
 
